bc32afa20b4062196b3f911cb6cbea8e57abac26
Maral Erfanian <me.maralerfanian@gmail.com>
2024-06-09 21:05:53 +0200

    home page styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,53 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.scss";
 
 function App() {
-	const [count, setCount] = useState(0);
+	// const [count, setCount] = useState(0);
 
 	return (
-		<div className="section">
-			<div className="background-image">
-				<div className="row">
-					<div className="flexRow">
-						<div className="box-shadow box box1">
+		<div className="container">
+			<div className="flexRow">
+				<div className="flexColumn">
+						<div className="box box3">
 							<div className="topHeader">
 								<h1 className="title">Home</h1>
 							</div>
 						</div>
-						<div className="box-shadow box box2">
+						<div className="box box4">
 							<div className="topHeader">
-								<h1 className="title">About me</h1>
-							</div>
-						</div>
-						<div className="flexColumn">
-							<div className="box-shadow box box3">
-								<div className="topHeader">
-									<h1 className="title">Projects</h1>
-								</div>
-							</div>
-							<div className="box-shadow box box4">
-								<div className="topHeader">
-									<h1 className="title">Contact</h1>
-								</div>
+								<h1 className="title">Contact</h1>
 							</div>
 						</div>
 					</div>
-					<div className="flexRow">
-						<div className="box-shadow box box5">
-							<div className="topHeader">
-								<h1 className="title">Skills</h1>
-							</div>
+					<div className="box box2">
+						<div className="topHeader">
+							<h1 className="title">About me</h1>
+						</div>
+					</div>
+					<div className="box">
+						<div className="topHeader">
+							<h1 className="title">Projects</h1>
 						</div>
 					</div>
 				</div>
-			</div>
-			{/* <div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-			</div> */}
+				<div className="flexRow">
+					<div className="box box5">
+						<div className="topHeader">
+							<h1 className="title">Skills</h1>
+						</div>
+					</div>
+				</div>
 		</div>
 	);
 }
 
 export default App;
+
+{/* <>
+	<div className="card">
+		<button onClick={() => setCount((count) => count + 1)}>
+			count is {count}
+		</button>
+	</div>
+	;
+</>; */}
